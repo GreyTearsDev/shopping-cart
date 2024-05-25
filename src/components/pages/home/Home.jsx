17a1c710dcb989afa06f8ef1833257c806024d7c
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-25 16:34:34 +0100

    style: wrap button in Link for navigation
    - Replaced the button element with a Link component from 'react-router-dom' to ensure the entire button area is clickable for navigation.
    - This change improves user experience by providing a larger clickable area for the 'Shop now' button.

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -4,9 +4,10 @@ export default function Home() {
   return (
     <>
       <h1>EXPLORE OUR STORE</h1>
-      <button>
-        <Link to="products">Shop now</Link>
-      </button>
+
+      <Link to="products">
+        <button>Shop now</button>
+      </Link>
     </>
   );
 }
