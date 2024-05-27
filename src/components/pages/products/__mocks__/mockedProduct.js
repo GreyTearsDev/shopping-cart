819501fe66f0ab4760aa2c5e2db2a436be430910
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-27 20:03:05 +0100

    feat: Add mockedProduct object for testing

---
+++ src/components/pages/products/__mocks__/mockedProduct.js
@@ -0,0 +1,11 @@
+
+const mockedProduct = {
+  id: 1,
+  title: "productTitle",
+  price: 10,
+  category: "productCategory",
+  description: "productDescription",
+  image: "productImageURL",
+};
+ 
+export default mockedProduct;
