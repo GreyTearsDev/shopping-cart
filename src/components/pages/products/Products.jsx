fb10ba979e1f061edc1b18b1e0ff4d48ec1eb27a
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-27 20:06:28 +0100

    refactor: Update Products component with filtering functionality
    - Added buttons for filtering products by category.
    - Implemented filterData function to update displayed products based on selected category.
    - Used ProductCard to display product details.
    - Displayed LoadingSpinner while data is loading.

--- src/components/pages/products/Products.jsx
+++ src/components/pages/products/Products.jsx
@@ -1,3 +1,35 @@
+import { useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import Button from "../../shared/Button";
+import LoadingSpinner from "../../shared/LoadingSpinner";
+import ProductCard from "./ProductCard";
+
 export default function Products() {
-  return <p>I am the Products</p>;
+  const [storeData, isLoading] = useOutletContext();
+  const [filteredData, setFilteredData] = useState(storeData);
+
+  const filterData = (query) => {
+    if (query === "all") {
+      setFilteredData(storeData);
+    } else {
+      setFilteredData(storeData.filter(product => product.category === query));
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <Button type="normal" text="All" onClick={() => filterData("all")} />
+        <Button type="normal" text="Men's Clothing" onClick={() => filterData("men's clothing")} />
+        <Button type="normal" text="Women's Clothing" onClick={() => filterData("women's clothing")} />
+        <Button type="normal" text="Electronics" onClick={() => filterData("electronics")} />
+        <Button type="normal" text="Jewelery" onClick={() => filterData("jewelery")} />
+      </div>
+      <div>
+        {isLoading
+          ? <LoadingSpinner />
+          : filteredData.map(product => <ProductCard key={product.id} product={product} />)}
+      </div>
+    </>
+  );
 }
