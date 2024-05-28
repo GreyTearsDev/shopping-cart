fd33082782e39974463f5c8528b2ebf2095ce0e8
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-28 10:55:22 +0100

    test: Add tests for ProductsFilter component
    - Added tests to ensure ProductsFilter renders a button with the correct text.
    - Implemented a test to verify that the filterData function is called when the button is clicked.

---
+++ src/components/pages/products/ProductsFilter.test.jsx
@@ -0,0 +1,31 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { afterEach, describe, expect, it, vi } from "vitest";
+import ProductsFilter from "./ProductsFilter";
+
+afterEach(() => {
+  cleanup();
+});
+
+describe("ProductsFilter component", () => {
+  it("should render a button with the text 'all'", () => {
+    render(<ProductsFilter filterData={() => {}} keyWords={["all"]} />);
+
+    const button = screen.getByRole("button", { name: /all/i });
+
+    expect(button).toBeInTheDocument();
+  });
+
+  it("should call 'filterData' when the button is clicked", async () => {
+    const user = userEvent.setup();
+    const filterData = vi.fn();
+
+    render(<ProductsFilter filterData={filterData} keyWords={["all"]} />);
+
+    const button = screen.getByRole("button", { name: "All" });
+
+    await user.click(button);
+
+    expect(filterData).toHaveBeenCalledOnce();
+  });
+});
