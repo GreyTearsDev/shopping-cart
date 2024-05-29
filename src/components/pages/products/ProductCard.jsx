821e203f1f7381f6b206489fc8a0fc9db61f9eb2
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-29 07:31:24 +0100

    feat: Enhance ProductCard component
    - Added product image rendering.
    - Incorporated product rating display using getStars utility function.
    - Updated heading structure for better semantics.

--- src/components/pages/products/ProductCard.jsx
+++ src/components/pages/products/ProductCard.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import { useOutletContext } from "react-router-dom";
 import addProductToCart from "../../../util/addToProductToCart";
+import getStars from "../../../util/getStars";
 import Button from "../../shared/Button";
 
 export default function ProductCard({ product }) {
@@ -9,9 +10,16 @@ export default function ProductCard({ product }) {
   const addToCart = () => {
     addProductToCart(product, cart, setCart);
   };
+
   return (
     <article>
-      <h3>{product.title}</h3>
+      <div>
+        <img src={product.image} alt={product.title} />
+      </div>
+      <h2>{product.title}</h2>
+      <div>
+        <p>{`${getStars(product.rating.rate)} (${product.rating.count} reviews)`}</p>
+      </div>
       <h3>{product.price} $</h3>
       <Button type="normal" text="Add to cart" onClick={addToCart} />
     </article>
