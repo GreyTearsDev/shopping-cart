99d8a21af5688d1fce036e7d31f8c792b806f4a8
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-06-01 09:15:29 +0100

    feat: Implement ProductDetails component
    - Added import statements for necessary dependencies.
    - Used useOutletContext to get store data and cart context.
    - Implemented addToCart function.
    - Rendered product details including image, title, price, rating, and description.
    - Added Button component to add product to cart.

--- src/components/pages/product-details/ProductDetails.jsx
+++ src/components/pages/product-details/ProductDetails.jsx
@@ -1,3 +1,39 @@
+import { useLocation, useParams } from "react-router-dom";
+import { useOutletContext } from "react-router-dom";
+import addProductToCart from "../../../util/addToProductToCart";
+import findProduct from "../../../util/findProduct";
+import getStars from "../../../util/getStars";
+import Button from "../../shared/Button";
+
 export default function ProductDetails() {
-  return <p>I am the ProductDetails</p>;
+  const [storeData, , cart, setCart] = useOutletContext();
+  const { productId } = useParams();
+  const product = findProduct(parseInt(productId), storeData);
+
+  const addToCart = () => {
+    addProductToCart(product, cart, setCart);
+  };
+
+  return (
+    <main className="product-details">
+      {product && (
+        <>
+          <div className="img-container product-details__img-container">
+            <img src={product.image} alt={product.title} />
+          </div>
+          <div className="product-info product-info__product-details">
+            <h2 className="font-xl">{product.price} $</h2>
+            <h3 className="font-l">{product.title}</h3>
+            <div className="rating">
+              <p className="font-m">{`${getStars(product.rating.rate)} (${product.rating.count} reviews)`}</p>
+            </div>
+            <p className="font-s">{product.description}</p>
+          </div>
+          <div className="product-details__product-actions">
+            <Button type="normal" text="Add to cart" onClick={addToCart} />
+          </div>
+        </>
+      )}
+    </main>
+  );
 }
