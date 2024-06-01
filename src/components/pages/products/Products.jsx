99e7d6716739a5272ad3058d7f27341855e28c3a
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-06-01 09:08:32 +0100

    refactor: Add error handling and performance improvements
    - Added error handling to display an error message when data fetching fails.
    - Used useCallback for filterData function to optimize performance.
    - Utilized useEffect to initialize filteredData when storeData changes.
    - Simplified JSX structure for better readability.

--- src/components/pages/products/Products.jsx
+++ src/components/pages/products/Products.jsx
@@ -1,30 +1,44 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useOutletContext } from "react-router-dom";
+import ErrorMessage from "../../shared/ErrorMessage";
 import LoadingSpinner from "../../shared/LoadingSpinner";
 import ProductCard from "./ProductCard";
 import ProductsFilter from "./ProductsFilter";
 
 export default function Products() {
-  const [storeData, isLoading] = useOutletContext();
+  const [storeData, isLoading, , , error] = useOutletContext();
   const [filteredData, setFilteredData] = useState(storeData);
   const keyWords = ["all", "men's clothing", "women's clothing", "electronics", "jewelery"];
 
-  const filterData = (query) => {
+  useEffect(() => {
+    if (!storeData) return;
+    setFilteredData([...storeData]);
+  }, [storeData]);
+
+  const filterData = useCallback((query) => {
+    if (isLoading) return;
     if (query === "all") {
       setFilteredData(storeData);
     } else {
       setFilteredData(storeData.filter(product => product.category === query));
     }
-  };
+  }, [storeData, isLoading]);
 
   return (
-    <>
-      <ProductsFilter filterData={filterData} keyWords={keyWords} />
-      <div>
-        {isLoading
-          ? <LoadingSpinner />
-          : filteredData.map(product => <ProductCard key={product.id} product={product} />)}
-      </div>
-    </>
+    isLoading
+      ? <LoadingSpinner />
+      : error
+      ? <ErrorMessage message="There was an error while fetching the data. Please, refresh the page." />
+      : (
+        <main>
+          <ProductsFilter filterData={filterData} keyWords={keyWords} />
+          {filteredData
+            && (
+              <section>
+                {filteredData.map(product => <ProductCard key={product.id} product={product} />)}
+              </section>
+            )}
+        </main>
+      )
   );
 }
