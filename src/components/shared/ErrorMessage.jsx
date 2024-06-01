c4a8df4517a5ea054acb8cab3e8974de95b4ff64
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-06-01 09:20:05 +0100

    feat: Add PropTypes

--- src/components/shared/ErrorMessage.jsx
+++ src/components/shared/ErrorMessage.jsx
@@ -1,3 +1,9 @@
+import PropTypes from "prop-types";
+
 export default function ErrorMessage({ message = "Error 404: Page not found" }) {
   return <h2>{message}</h2>;
 }
+
+ErrorMessage.propTypes = {
+  message: PropTypes.string,
+};
