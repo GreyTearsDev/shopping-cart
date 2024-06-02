54cb6171fc86d9b1d19801decd4cd1d1692320bd
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-06-02 23:46:00 +0100

    feat: Implement AmountSetter component with validation
    - Manages and displays an amount with increment, decrement, and custom input options.
    - Added PropTypes for setAmount and amount props to ensure correct usage.
    - Implemented custom hook useAmountSetter to handle amount validation and actions.

---
+++ src/components/shared/AmountSetter.jsx
@@ -0,0 +1,86 @@
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import Button from "./Button";
+
+export default function AmountSetter({ amount, setAmount }) {
+  const [displayAmount, setDisplayAmount] = useState(amount);
+  const { error, incrementAmount, decrementAmount, setCustomAmount } = useAmountSetter(amount, setAmount);
+
+  useEffect(() => {
+    setDisplayAmount(amount);
+  }, [amount]);
+
+  return (
+    <div>
+      {error && <p>{error}</p>}
+      <Button type="normal" text="-" onClick={decrementAmount} />
+      <input value={displayAmount.toString()} onChange={(e) => setCustomAmount(e.target.value)} pattern="\d+" />
+      <Button type="normal" text="+" onClick={incrementAmount} />
+    </div>
+  );
+}
+
+AmountSetter.propTypes = {
+  setAmount: PropTypes.func,
+  amount: PropTypes.number,
+};
+
+function useAmountSetter(amount, setAmount) {
+  const [error, setError] = useState(null);
+
+  const amountIsValid = (action, amount) => {
+    switch (action) {
+      case "increment":
+        return amount <= 1000000;
+      case "decrement":
+        return amount > 1;
+      case "setCustom":
+        return amount > 0 && amount <= 1000000;
+      default:
+        return false;
+    }
+  };
+
+  const incrementAmount = () => {
+    if (amountIsValid("increment", amount)) {
+      setAmount(amount + 1);
+      setError(null);
+    } else {
+      setError("Maximum amount exceeded. You can only purchase up to 1.000.000 (one million) items at a time");
+    }
+  };
+
+  const decrementAmount = () => {
+    if (amountIsValid("decrement", amount)) {
+      setAmount(amount - 1);
+      setError(null);
+    } else {
+      setError("The minimum amount for purchasing is 1");
+    }
+  };
+
+  const setCustomAmount = (value) => {
+    if (value === "") {
+      setError("The minimum amount for purchasing is 1");
+      return;
+    }
+
+    if (!(/^\d+$/.test(value))) {
+      setError("Only numbers are allowed");
+      return;
+    } else {
+      setError(null);
+    }
+
+    const parsedAmount = parseInt(value, 10);
+
+    if (amountIsValid("setCustom", parsedAmount)) {
+      setAmount(parsedAmount);
+      setError(null);
+    } else {
+      setError("Invalid amount. Type a number from 1 to 1.000.000");
+    }
+  };
+
+  return { error, incrementAmount, decrementAmount, setCustomAmount };
+}
