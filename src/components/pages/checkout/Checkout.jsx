d3a0eae27bcda6a428c0d3f3a2b4eea7427bbe35
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-06-03 21:43:02 +0100

    fix: Fix typo in the name of 'addToProductToCart.js' to 'addProductToCart.js'

--- src/components/pages/checkout/Checkout.jsx
+++ src/components/pages/checkout/Checkout.jsx
@@ -1,3 +1,20 @@
+import { useEffect, useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import Summary from "./Summary";
+
 export default function Checkout() {
-  return <p>I am the Checkout</p>;
+  const [storeData, , cart, setCart] = useOutletContext();
+  const [billTotal, setBillTotal] = useState(0);
+
+  useEffect(() => {
+    const total = cart.map(product => product.price * product.amount)
+      .reduce((prev, curr) => prev + curr, 0);
+    setBillTotal(total);
+  }, [cart]);
+
+  const onPlaceOrder = () => {
+    setCart([]);
+  };
+
+  return <Summary billTotal={billTotal} onPlaceOrder={onPlaceOrder} />;
 }
