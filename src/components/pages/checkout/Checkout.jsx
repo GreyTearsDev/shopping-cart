b30f217efadb34092e38341c937dd5100e124a83
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-06-04 06:42:15 +0100

    refactor: Update Checkout component to use custom hook for bill and taxes calculation
    - Removed useEffect and useState hooks for bill total calculation
    - Added useBillAndTaxes custom hook to calculate bill total and taxes
    - Imported CheckedOutProducts component and included it in the Checkout component
    - Updated Summary component to receive and display both billTotal and taxesTotal
    - Refactored JSX structure to include CheckedOutProducts and Summary inside a main tag

--- src/components/pages/checkout/Checkout.jsx
+++ src/components/pages/checkout/Checkout.jsx
@@ -1,20 +1,19 @@
-import { useEffect, useState } from "react";
 import { useOutletContext } from "react-router-dom";
+import useBillAndTaxes from "../../../hooks/useBillAndTaxes";
+import CheckedOutProducts from "./CheckedOutProducts";
 import Summary from "./Summary";
 
 export default function Checkout() {
   const [storeData, , cart, setCart] = useOutletContext();
-  const [billTotal, setBillTotal] = useState(0);
-
-  useEffect(() => {
-    const total = cart.map(product => product.price * product.amount)
-      .reduce((prev, curr) => prev + curr, 0);
-    setBillTotal(total);
-  }, [cart]);
-
+  const { billTotal, taxesTotal } = useBillAndTaxes(cart);
   const onPlaceOrder = () => {
     setCart([]);
   };
 
-  return <Summary billTotal={billTotal} onPlaceOrder={onPlaceOrder} />;
+  return (
+    <main>
+      <CheckedOutProducts />
+      <Summary billTotal={billTotal} taxesTotal={taxesTotal} onPlaceOrder={onPlaceOrder} />
+    </main>
+  );
 }
