b68b00f4d95a39f172be2399614b99007c25307d
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-06-04 19:34:35 +0100

    refactor: Rename CheckedOutProducts component to CheckedOutProductsContainer in Checkout component
    - Renamed CheckedOutProducts to CheckedOutProductsContainer for better clarity
    - Updated import statement and usage in Checkout component accordingly

--- src/components/pages/checkout/Checkout.jsx
+++ src/components/pages/checkout/Checkout.jsx
@@ -1,18 +1,19 @@
 import { useOutletContext } from "react-router-dom";
 import useBillAndTaxes from "../../../hooks/useBillAndTaxes";
-import CheckedOutProducts from "./CheckedOutProducts";
+import CheckedOutProductsContainer from "./CheckedOutProductsContainer";
 import Summary from "./Summary";
 
 export default function Checkout() {
-  const [storeData, , cart, setCart] = useOutletContext();
+  const [, , cart, setCart] = useOutletContext();
   const { billTotal, taxesTotal } = useBillAndTaxes(cart);
+
   const onPlaceOrder = () => {
     setCart([]);
   };
 
   return (
     <main>
-      <CheckedOutProducts />
+      <CheckedOutProductsContainer />
       <Summary billTotal={billTotal} taxesTotal={taxesTotal} onPlaceOrder={onPlaceOrder} />
     </main>
   );
