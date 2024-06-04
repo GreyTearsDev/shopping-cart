6715b7d0b537b7048838f05433bb92d9b0c29f1c
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-06-04 20:04:51 +0100

    refactor: Utilize roundNumber utility in Summary component
    - Imported roundNumber utility function
    - Applied roundNumber function to billTotal, taxesTotal, and total amount calculations in Summary component

--- src/components/pages/checkout/Summary.jsx
+++ src/components/pages/checkout/Summary.jsx
@@ -1,13 +1,14 @@
 import PropTypes from "prop-types";
+import roundNumber from "../../../util/roundNumber";
 import Button from "../../shared/Button";
 
 export default function Summary({ billTotal, taxesTotal, onPlaceOrder }) {
   return (
     <section>
       <h2>Summary</h2>
-      <p>Subtotal: ${billTotal}</p>
-      <p>Taxes: ${taxesTotal}</p>
-      <h3>Total: ${Math.round(((billTotal + taxesTotal) + Number.EPSILON) * 100) / 100}</h3>
+      <p>Subtotal: ${roundNumber(billTotal)}</p>
+      <p>Taxes: ${roundNumber(taxesTotal)}</p>
+      <h3>Total: ${roundNumber(billTotal + taxesTotal)}</h3>
       <Button text="Place Order" onClick={onPlaceOrder} />
     </section>
   );
