9218c0f9fef14c9a837331bd566229b9f9f1ff5e
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-06-05 22:39:34 +0100

    refactor: Integrate ThemeProvider into main application rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./globalStyles";
 import routes from "./routes";
 
 const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
 );
