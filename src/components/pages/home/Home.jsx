5fa7d9d2ac62702f044dc639de86ef0943932b13
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-06-06 06:54:13 +0100

    refactor: Refactor Home component to use styled-components for layout
    - Import styled-components and define Wrapper styled component
    - Replace fragment with Wrapper component to wrap Home component content
    - Add styling to center content and add margin to button

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,11 +1,25 @@
+import styled from "styled-components";
 import Button from "../../shared/Button";
+
+const Wrapper = styled.main`
+padding: 6vw 0;
+height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  button {
+  margin-top: 1vw;
+}
+`;
 
 export default function Home() {
   return (
-    <>
+    <Wrapper>
       <h1>EXPLORE OUR STORE</h1>
 
       <Button type="link" path="products" text="Shop now" />
-    </>
+    </Wrapper>
   );
 }
