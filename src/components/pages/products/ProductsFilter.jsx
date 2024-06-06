25d24b7f82121631bb672e115c770f1c69396544
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-06-06 15:28:39 +0100

    style: Create 'Wrapper' styled component using 'syled-components' library

--- src/components/pages/products/ProductsFilter.jsx
+++ src/components/pages/products/ProductsFilter.jsx
@@ -1,6 +1,19 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
+import styled from "styled-components";
 import Button from "../../shared/Button";
+
+const Wrapper = styled.div`
+  display: flex;
+  // styles for when the screen gets too small
+  // display: grid;
+  // grid-template-column: repeat(auto, minmax(min-content, 1fr) );
+  justify-content: center;
+  align-content: center;
+  gap: 0.5vw;
+  padding: clamp(40px, 2vw, 80px) 0;
+      
+`;
 
 export default function ProductsFilter({ filterData, keyWords }) {
   const [activeFilter, setActiveFilter] = useState("all");
@@ -10,7 +23,7 @@ export default function ProductsFilter({ filterData, keyWords }) {
     setActiveFilter(word);
   };
   return (
-    <div>
+    <Wrapper>
       {keyWords.map(word => (
         <Button
           active={activeFilter === word ? true : false}
@@ -20,7 +33,7 @@ export default function ProductsFilter({ filterData, keyWords }) {
           onClick={() => handleClick(word)}
         />
       ))}
-    </div>
+    </Wrapper>
   );
 }
 
