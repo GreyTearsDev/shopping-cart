be5d4dd9e33305252da2175ddd5ff8c2e8d67070
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-06-06 19:16:34 +0100

    refactor: Refactor AmountSetter component's layout
    - Move AmountSetter component definition to the top of the file
    - Adjust Wrapper styles for responsive grid layout
    - Simplify Button and Input styles for consistency

--- src/components/shared/AmountSetter.jsx
+++ src/components/shared/AmountSetter.jsx
@@ -3,48 +3,6 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 import useAmountSetter from "../../hooks/useAmountSetter";
 
-const Wrapper = styled.div`
-  width: 200px;
-  border: 1px solid ${({ theme }) => theme.colors.primary};
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  gap: 10px;
-  margin: 10px 0;
-`;
-
-const Button = styled.button`
-  background-color: ${({ theme }) => theme.colors.primary};
-  width: 40px;
-  height: 40px;
-  color: ${({ theme }) => theme.colors.secondary};
-  border: none;
-  padding: 10px;
-  border-radius: 5px;
-  font-size: 1rem;
-  cursor: pointer;
-  transition: background-color 200ms ease, transform 100ms ease;
-
-  &:hover {
-    background-color: ${({ theme }) => theme.colors.secondary};
-    color: ${({ theme }) => theme.colors.primary};
-  }
-
-  &:active {
-    transform: scale(0.95);
-  }
-`;
-
-const Input = styled.input`
-flex: 1;
-  width: 50px;
-  text-align: center;
-  padding: 10px;
-  border: 1px solid ${({ theme }) => theme.colors.primary};
-  border-radius: 5px;
-  font-size: 1rem;
-`;
-
 export default function AmountSetter({ amount, setAmount }) {
   const [displayAmount, setDisplayAmount] = useState(amount);
   const { error, incrementAmount, decrementAmount, setCustomAmount } = useAmountSetter(amount, setDisplayAmount);
@@ -71,3 +29,42 @@ AmountSetter.propTypes = {
   setAmount: PropTypes.func.isRequired,
   amount: PropTypes.number.isRequired,
 };
+
+const Wrapper = styled.div`
+  width: fit-content;
+  display: grid;
+  grid-template-columns: minmax(40px, 25%) 1fr minmax(40px, 25%);
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  margin: 10px 0;
+`;
+
+const Button = styled.button`
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.secondary};
+  border: none;
+  padding: 10px;
+  border-radius: 5px;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: background-color 200ms ease, transform 100ms ease;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.secondary};
+    color: ${({ theme }) => theme.colors.primary};
+  }
+
+  &:active {
+    transform: scale(0.95);
+  }
+`;
+
+const Input = styled.input`
+  flex: 1;
+  text-align: center;
+  padding: 10px;
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+  border-radius: 5px;
+  font-size: 1rem;
+`;
