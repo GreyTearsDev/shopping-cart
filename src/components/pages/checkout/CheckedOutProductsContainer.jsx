94b98519d307e2ddb8af4140d6157e5e6473e4e1
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-06-06 12:08:39 +0100

    refactor: Add fallback jsx for when the cart is empty

--- src/components/pages/checkout/CheckedOutProductsContainer.jsx
+++ src/components/pages/checkout/CheckedOutProductsContainer.jsx
@@ -1,9 +1,12 @@
 import { useOutletContext } from "react-router-dom";
 import findProduct from "../../../util/findProduct";
+import Button from "../../shared/Button";
 import CheckedOutProductCard from "./CheckedOutProductCard";
 
 export default function CheckedOutProductsContainer() {
   const [storeData, , cart, setCart] = useOutletContext();
+  const cartIsEmpty = cart.length <= 0;
+
   const onDelete = (product) => {
     const newCart = cart.filter(p => p.id !== product.id);
     setCart(newCart);
@@ -11,17 +14,25 @@ export default function CheckedOutProductsContainer() {
 
   return (
     <section>
-      {cart.map(checkedOutProduct => {
-        const storedProduct = findProduct(checkedOutProduct.id, storeData);
-        return (
-          <CheckedOutProductCard
-            key={storedProduct.id}
-            product={storedProduct}
-            onDelete={onDelete}
-            productAmount={checkedOutProduct.amount}
-          />
-        );
-      })}
+      {!cartIsEmpty
+        ? cart.map(checkedOutProduct => {
+          const storedProduct = findProduct(checkedOutProduct.id, storeData);
+          return (
+            <CheckedOutProductCard
+              key={storedProduct.id}
+              product={storedProduct}
+              onDelete={onDelete}
+              productAmount={checkedOutProduct.amount}
+            />
+          );
+        })
+        : (
+          <>
+            <h2>Your cart is empty!</h2>
+
+            <Button type="link" path="products" text="Shop now" />
+          </>
+        )}
     </section>
   );
 }
