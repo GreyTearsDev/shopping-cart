d4d6257a8ac74d386ea8262179ca85d25622c681
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-06-06 12:10:40 +0100

    style: Create styled componets to style the product cards

--- src/components/pages/products/ProductCard.jsx
+++ src/components/pages/products/ProductCard.jsx
@@ -1,8 +1,41 @@
 import PropTypes from "prop-types";
 import { Link, useOutletContext } from "react-router-dom";
+import styled from "styled-components";
 import addProductToCart from "../../../util/addProductToCart";
 import getStars from "../../../util/getStars";
 import Button from "../../shared/Button";
+
+const Wrapper = styled.article`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: clamp(4px, 1vw, 20px) clamp(4px, 2vw, 20px);
+  background-color: ${({ theme }) => theme.colors.ternary};
+  border-radius: 0.9rem;
+
+  button {
+  margin-top: 30px;
+}
+`;
+
+const ImageWrapper = styled.div`
+  background-color: ${({ theme }) => theme.colors.secondary};
+  padding: 20px;
+  display: flex;
+  border-radius: 0.9rem;
+  justify-content: center;
+
+  img {
+    object-fit: contain;
+  }
+`;
+
+const InfoWrapper = styled.div`
+  height: max-content;
+  display: grid;
+  grid-template-rows: 300px repeat(3, max-content);
+  marign-bottom: 200px;
+`;
 
 export default function ProductCard({ product }) {
   const [, , cart, setCart] = useOutletContext();
@@ -12,21 +45,21 @@ export default function ProductCard({ product }) {
   };
 
   return (
-    <article>
+    <Wrapper>
       <Link to={`/product-details/${product.id}`}>
-        <div>
-          <div>
+        <InfoWrapper>
+          <ImageWrapper>
             <img src={product.image} alt={product.title} />
-          </div>
+          </ImageWrapper>
           <h2>{product.title}</h2>
           <div>
             <p>{`${getStars(product.rating.rate)} (${product.rating.count} reviews)`}</p>
           </div>
           <h3>${product.price}</h3>
-        </div>
+        </InfoWrapper>
       </Link>
       <Button type="normal" text="Add to cart" onClick={addToCart} />
-    </article>
+    </Wrapper>
   );
 }
 
