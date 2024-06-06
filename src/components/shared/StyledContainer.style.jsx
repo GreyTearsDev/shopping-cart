04e3bc75a5e1ae8260cc029d4a4174c3d9a018b9
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-06-06 06:56:21 +0100

    refactor: Create StyledContainer component using styled-components
    - Import styled-components
    - Define and export StyledContainer styled component with flex layout and responsive design

---
+++ src/components/shared/StyledContainer.style.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
+  display: flex;
+  max-width: 1536px;
+  display: flex;
+  flex-direction: column;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 5.5rem;
+  padding-right: 5.5rem;
+
+`;
