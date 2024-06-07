ec6dbf40b44b7e35c96a4426b99b635348712f7a
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-06-07 12:00:25 +0100

    style: Style the Summary component using the 'styled-componets' library

--- src/components/pages/checkout/Summary.jsx
+++ src/components/pages/checkout/Summary.jsx
@@ -1,16 +1,38 @@
 import PropTypes from "prop-types";
+import styled from "styled-components";
 import roundNumber from "../../../util/roundNumber";
 import Button from "../../shared/Button";
 
+const StyledSummary = styled.section`
+  position: sticky;
+  top: 6vw ;
+  border-left: 1px solid ${({ theme }) => theme.colors.primary};
+  height: 100vh;
+  text-align: center;
+  padding: 2rem;
+  z-index: 0;
+
+  h2, h3 {
+    margin-bottom: 1rem;
+  }
+
+  p {
+    margin: 0;
+  }
+  @media (max-width: 920px) {
+    border: none;
+  }
+`;
+
 export default function Summary({ billTotal, taxesTotal, onPlaceOrder }) {
   return (
-    <section>
+    <StyledSummary>
       <h2>Summary</h2>
       <p>Subtotal: ${roundNumber(billTotal)}</p>
       <p>Taxes: ${roundNumber(taxesTotal)}</p>
       <h3>Total: ${roundNumber(billTotal + taxesTotal)}</h3>
       <Button type="normal" text="Place Order" onClick={onPlaceOrder} />
-    </section>
+    </StyledSummary>
   );
 }
 
