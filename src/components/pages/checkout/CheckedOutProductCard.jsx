c9368cca869283e25e5a34968a8e16d814f649ef
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-06-07 12:02:22 +0100

    style: Style the CheckedOutProductCard component using the 'styled-componets' library

--- src/components/pages/checkout/CheckedOutProductCard.jsx
+++ src/components/pages/checkout/CheckedOutProductCard.jsx
@@ -1,9 +1,58 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
 import { useOutletContext } from "react-router-dom";
+import styled from "styled-components";
 import addProductToCart from "../../../util/addProductToCart";
 import AmountSetter from "../../shared/AmountSetter";
 import Button from "../../shared/Button";
+
+const ArticleWrapper = styled.article`
+  background-color: ${({ theme }) => theme.colors.secondary};
+  display: grid;
+  grid-template-columns: minmax(200px, 25%) 1fr;
+  border: 1px solid ${({ theme }) => theme.colors.ternary};
+  padding: 0.5rem 1rem;
+  margin: clamp(20px, 2vw, 40px) 0;
+
+  @media (max-width: 920px) {
+    grid-template-columns: 1fr;
+    grid-template-rows: auto auto;
+  }
+`;
+
+const ImageWrapper = styled.div`
+  background-color: ${({ theme }) => theme.colors.secondary};
+  display: flex;
+  justify-self: center;
+  max-width: 60%;
+  align-self: center;
+
+  img {
+    object-fit: contain;
+  }
+`;
+
+const ButtonsWrapper = styled.div`
+  margin-top: 1rem;
+  display: flex;
+  align-items: center;
+  gap: 3vw;  
+`;
+
+const InfoWrapper = styled.div`
+  height: max-content;
+  display: grid;
+  grid-template-rows: repeat(auto-fill, max-content);
+  marign-bottom: 200px;
+
+  p {
+    margin: 0 0 1rem 0;
+  }
+
+  @media (max-width: 920px) {
+      align-items: center;
+  }
+`;
 
 export default function CheckedOutProductCard({ product, onDelete, productAmount }) {
   const [amount, setAmount] = useState(parseInt(productAmount));
@@ -14,16 +63,22 @@ export default function CheckedOutProductCard({ product, onDelete, productAmount
   };
 
   return (
-    <article>
-      <button onClick={() => onDelete(product)}>x</button>
-      <div>
+    <ArticleWrapper>
+      <ImageWrapper>
         <img src={product.image} alt={product.title} />
-      </div>
-      <h3>{product.title}</h3>
-      <p>Price: ${product.price}</p>
-      <AmountSetter amount={amount} setAmount={setAmount} />
-      <Button type="normal" text="Update Bill" onClick={addToCart} />
-    </article>
+      </ImageWrapper>
+
+      <InfoWrapper>
+        <h3>{product.title}</h3>
+        <p>Price: ${product.price}</p>
+        <AmountSetter amount={amount} setAmount={setAmount} />
+
+        <ButtonsWrapper>
+          <Button type="normal" text="Update Bill" onClick={addToCart} />
+          <Button type="normal" text="Remove Item" onClick={() => onDelete(product)} />
+        </ButtonsWrapper>
+      </InfoWrapper>
+    </ArticleWrapper>
   );
 }
 
