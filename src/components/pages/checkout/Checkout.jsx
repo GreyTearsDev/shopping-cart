ed7c18d64386370da3515b8f6a8fea2bce29824f
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-06-07 12:01:19 +0100

    style: Style the Checkout component using the 'styled-componets' library

--- src/components/pages/checkout/Checkout.jsx
+++ src/components/pages/checkout/Checkout.jsx
@@ -1,7 +1,19 @@
 import { useOutletContext } from "react-router-dom";
+import styled from "styled-components";
 import useBillAndTaxes from "../../../hooks/useBillAndTaxes";
 import CheckedOutProductsContainer from "./CheckedOutProductsContainer";
 import Summary from "./Summary";
+
+const Wrapper = styled.main`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: minmax(400px, 3fr) minmax(200px, 1fr);
+
+  @media (max-width: 800px) {
+    grid-template-columns: 1fr;
+    grid-template-rows: auto auto;
+
+  }`;
 
 export default function Checkout() {
   const [, , cart, setCart] = useOutletContext();
@@ -12,9 +24,9 @@ export default function Checkout() {
   };
 
   return (
-    <main>
+    <Wrapper>
       <CheckedOutProductsContainer />
       <Summary billTotal={billTotal} taxesTotal={taxesTotal} onPlaceOrder={onPlaceOrder} />
-    </main>
+    </Wrapper>
   );
 }
