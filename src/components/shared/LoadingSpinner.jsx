9b0f61536d55fc96f2f05680744af38f3d1b294e
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-06-07 11:51:03 +0100

    style: Add basic styling to LoadingSpinner component using styled-componets

--- src/components/shared/LoadingSpinner.jsx
+++ src/components/shared/LoadingSpinner.jsx
@@ -1,3 +1,19 @@
+import styled from "styled-components";
+
+const Wrapper = styled.main`
+  padding: 6vw 0;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+`;
+
 export default function LoadingSpinner() {
-  return <p>Loading</p>;
+  return (
+    <Wrapper>
+      <h2>Loading...</h2>
+    </Wrapper>
+  );
 }
